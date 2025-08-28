a0238be7851e6dc39813ae330998aef98e9566c8
Trisha Goodwin <trishabromero@gmail.com>
2025-08-28 14:13:37 -0500

    few nav updates, setting up portfolio

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,9 +1,13 @@
 import { Routes,Router } from "react-router-dom"
 
+
 function Portfolio() {
   return (
     <>
     <div className="portfolio">
+        <div>
+          Capstone: Bandit Pals
+        </div>
         <div>
             <p>Portfoliio will go here!</p>
         </div>
